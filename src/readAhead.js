109684cfca02823a78868557b7c35a7a09d70ced
Torbjørn Nilsen <torbjorn.nilsen@gmail.com>
2016-07-29 22:16:09 +1000

    Fix whitespace bug.

--- src/readAhead.js
+++ src/readAhead.js
@@ -3,7 +3,7 @@ class ReadAhead{
     static read(value, matcher, cursor, ignoreWhitespace = false){
         let internalCursor = cursor;
         for(let i = 0; i < matcher.length && i < value.length ; i++){
-            while(value.charAt(internalCursor) == ' '){
+            while(ignoreWhitespace && value.charAt(internalCursor) == ' '){
                 internalCursor++;
             }
             if(value.charAt(internalCursor) == matcher.charAt(i)){
